f6c488c0444456a3b86126f40bdf9362213ac68f
spaceboundbear <mf.michaelfischer@gmail.com>
2021-12-16 02:30:26 -0600

    footer layout, sticky

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,22 @@ import React from 'react';
 
 function Footer() {
   return (
-    <header className="header">
-      <h1>Footer Placeholder</h1>
-    </header>
+    <footer className="w-100 mt-auto bg-secondary p-4 footer fixed-bottom">
+      <div className="container text-center mb-5">
+        <h4>
+          Made with{' '}
+          <span
+            className="emoji"
+            role="img"
+            aria-label="heart"
+            aria-hidden="false"
+          >
+            ❤️
+          </span>{' '}
+          by the Tech Thoughts team.
+        </h4>
+      </div>
+    </footer>
   );
 }
 

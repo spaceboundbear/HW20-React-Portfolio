39d06e05a6734dc0c2b246d435d29377943c8ed2
spaceboundbear <mf.michaelfischer@gmail.com>
2021-12-17 06:22:24 -0600

    active nav links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,17 +3,26 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import 'bootstrap/dist/css/bootstrap.css';
+import { NavLink } from 'react-router-dom';
 
 function topNav() {
   return (
     <Navbar bg="dark" variant="dark" className="sticky-top">
       <Container>
-        <Navbar.Brand href="/about">Michael Fischer</Navbar.Brand>
+        <Navbar.Brand to="/about">Michael Fischer</Navbar.Brand>
         <Nav className="justify-content-end">
-          <Nav.Link href="/about">About</Nav.Link>
-          <Nav.Link href="/portfolio">Portfolio</Nav.Link>
-          <Nav.Link href="/contact">Contact</Nav.Link>
-          <Nav.Link href="/resume">Resume</Nav.Link>
+          <NavLink className=" nav-link" to="/about">
+            About
+          </NavLink>
+          <NavLink className=" nav-link" to="/portfolio">
+            Portfolio
+          </NavLink>
+          <NavLink className=" nav-link" to="/contact">
+            Contact
+          </NavLink>
+          <NavLink className=" nav-link" to="/resume">
+            Resume
+          </NavLink>
         </Nav>
       </Container>
     </Navbar>
